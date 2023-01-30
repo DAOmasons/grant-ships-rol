2221cf6b5dd5f51e434c5517780f6920a1a031c9
jordan <jordan.lesich@gmail.com>
2023-01-29 19:02:13 -0800

    app structure

--- src/Routes.tsx
+++ src/Routes.tsx
@@ -2,6 +2,10 @@ import { DHLayout } from '@daohaus/connect';
 import { Routes as Router, Route, useLocation } from 'react-router-dom';
 import { FormTest } from './pages/FormTest';
 import { Home } from './pages/Home';
+import { Leaderboard } from './pages/Leaderboard';
+import { Ritual } from './pages/Ritual';
+import { Settings } from './pages/Settings';
+import { Timeline } from './pages/Timeline';
 
 export const Routes = () => {
   const { pathname } = useLocation();
@@ -10,12 +14,18 @@ export const Routes = () => {
       pathname={pathname}
       navLinks={[
         { label: 'Home', href: '/' },
-        { label: 'Form Test', href: '/formtest' },
+        { label: 'Timeline', href: '/timeline' },
+        { label: 'Claim', href: '/claim' },
+        { label: 'Leaderboard', href: '/leaderboard' },
+        { label: 'Settings', href: '/settings' },
       ]}
     >
       <Router>
         <Route path="/" element={<Home />} />
-        <Route path="/formtest" element={<FormTest />} />
+        <Route path="/timeline" element={<Timeline />} />
+        <Route path="/claim" element={<Ritual />} />
+        <Route path="/leaderboard" element={<Leaderboard />} />
+        <Route path="/settings" element={<Settings />} />
       </Router>
     </DHLayout>
   );
---
+++ src/pages/Ritual.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+
+export const Ritual = () => {
+  return <div>Ritual</div>;
+};
