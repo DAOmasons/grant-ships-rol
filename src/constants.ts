63d5f8d268ca9a8db03300d1742708080084f5b9
jordan <jordan.lesich@gmail.com>
2023-02-03 15:58:08 -0800

    validate ismember, isConnected, isLocked, isCorrectnetwork on Claims page. Create Warning UI.

--- src/constants.ts
+++ src/constants.ts
@@ -1,8 +1,17 @@
+import { ValidNetwork } from '@daohaus/keychain-utils';
+import { EthAddress } from '@daohaus/utils';
+
 export const SHAMAN_GRAPH_ENDPOINT =
   'https://api.thegraph.com/subgraphs/name/jordanlesich/ritualsacrificeshaman';
 
-export const TARGET_DAO = {
+export const TARGET_DAO: {
+  ADDRESS: EthAddress;
+  CHAIN_ID: ValidNetwork;
+  CHAIN_NAME: string;
+  ROS_V2_SHAMAN: EthAddress;
+} = {
   ADDRESS: '0x853fea54aa815d32ecef67bbb15dc31da963b2d5',
   CHAIN_ID: '0x64',
+  CHAIN_NAME: 'Gnosis Chain',
   ROS_V2_SHAMAN: '0xf1972b7801c4a479fb79598534dc055406db75a1',
 };
