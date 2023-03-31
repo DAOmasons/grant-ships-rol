c03565edee09b151ae5a197675d74a7a6df73677
jordan <jordan.lesich@gmail.com>
2023-03-31 14:15:09 -0700

    make zod validators, not using them yet

--- src/constants.ts
+++ src/constants.ts
@@ -27,6 +27,7 @@ const DemoProjectData: ProjectMetadata = {
     imageUrl:
       'https://cdn.discordapp.com/attachments/1087525376630198312/1088883720309710858/Bjav6ge.png',
   },
+  rubricType: 'time-only',
   rubricDescription: [
     "Low effort/focus work. Ex. Casual research, low impact calls. Any work that doesn't require a lot of focus.",
     'Easy or simple work. Or high impact work where the worker is still learning and takes longer than usual. Ex. Organizing, simple refactors, hacking using a new language.',
@@ -34,6 +35,29 @@ const DemoProjectData: ProjectMetadata = {
     'High impact work. Ex. Baseline work done with someone with a lot of experience or work done that yields high impact or requires a lot of focus.',
     'Excellent work. High impact, high focus work. This work would require a lot of focus and experience to complete.',
   ],
+};
+
+const ArbitrumCampaignProjectData: ProjectMetadata = {
+  name: 'Arbitrum | DAO Builder Campaign',
+  description: 'DAO Masons hunting outreach program for DAOs on Arbitrum',
+  imageUrl: 'https://mma.prnewswire.com/media/1888305/Arbitrum_Logo.jpg?w=200',
+  mission:
+    "This project aims to promote our DAO's services to other DAOs within the Arbitrum ecosystem, increase brand awareness, and establish partnerships.",
+  links: [
+    {
+      name: 'Project Proposal',
+      url: 'https://hackmd.io/REwxi-dFQe29Z_-fvy7YNQ',
+    },
+  ],
+  dueDate: 1682713862,
+  rubricType: 'time-only',
+  parentDAOInfo: {
+    name: 'DAO Masons',
+    address: '0x5b448757a34402deacc7729b79003408cdfe1438',
+    description: 'DAO Masons is a DAO that helps other DAOs',
+    imageUrl:
+      'https://cdn.discordapp.com/attachments/1087525376630198312/1088883720309710858/Bjav6ge.png',
+  },
 };
 
 export const TARGET_DAO: {
@@ -47,27 +71,5 @@ export const TARGET_DAO: {
   CHAIN_ID: '0xa4b1',
   CHAIN_NAME: 'Arbitrum One',
   ROS_V2_SHAMAN: '0xD2dE8423608Ba1B14ad6d2c84aa00C75B3B39317',
-  PROJECT_DATA: DemoProjectData,
-};
-
-const ArbitrumCampaignProjectData = {
-  name: 'Arbitrum | DAO Builder Campaign',
-  description: 'DAO Masons hunting outreach program for DAOs on Arbitrum',
-  imageUrl: 'https://mma.prnewswire.com/media/1888305/Arbitrum_Logo.jpg?w=200',
-  mission:
-    "This project aims to promote our DAO's services to other DAOs within the Arbitrum ecosystem, increase brand awareness, and establish partnerships.",
-  links: [
-    {
-      name: 'Project Proposal',
-      url: 'https://hackmd.io/REwxi-dFQe29Z_-fvy7YNQ',
-    },
-  ],
-  dueDate: 1682713862,
-  parentDAOInfo: {
-    name: 'DAO Masons',
-    address: '0x5b448757a34402deacc7729b79003408cdfe1438',
-    description: 'DAO Masons is a DAO that helps other DAOs',
-    imageUrl:
-      'https://cdn.discordapp.com/attachments/1087525376630198312/1088883720309710858/Bjav6ge.png',
-  },
+  PROJECT_DATA: ArbitrumCampaignProjectData,
 };
--- src/types/timeline.ts
+++ src/types/timeline.ts
@@ -1,83 +1,118 @@
-export type EventData = {
-  id: string;
-  createdAt: string;
-  createdBy: string;
-};
-export type ClaimMetadata = {
-  claimDetails: string[];
-  morale: string;
-  future: string;
-  obstacles: string;
-};
-export type ProjectLink = {
-  name: string;
-  url: string;
-};
+import { z } from 'zod';
 
-export type Claim = EventData & {
-  type: 'claim';
-  metadata: 'Corrupt' | ClaimMetadata;
-  totalSecondsWorked: string;
-  totalAmountClaimed: string;
-  sessionsTime: string[];
-  sessionsValue: number[];
-};
+const EventValidator = z.object({
+  id: z.string(),
+  createdAt: z.string(),
+  createdBy: z.string(),
+});
+export type EventData = z.infer<typeof EventValidator>;
 
-export type SummonShaman = EventData & {
-  type: 'summon';
-  projectMetadata: ProjectMetadata | 'Corrupt';
-};
+const ClaimMetadataValidator = z.object({
+  claimDetails: z.array(z.string()),
+  morale: z.string(),
+  future: z.string(),
+  obstacles: z.string(),
+});
+export type ClaimMetadata = z.infer<typeof ClaimMetadataValidator>;
 
-export type UpdateLock = EventData & {
-  type: 'updateLock';
-  isLocked: boolean;
-};
+const ProjectLinkValidator = z.object({
+  name: z.string(),
+  url: z.string(),
+});
+export type ProjectLink = z.infer<typeof ProjectLinkValidator>;
 
-export type Mutiny = EventData & {
-  type: 'mutiny';
-  mutinyFrom: string;
-  mutinyTo: string;
-};
+const ProjectMetadataValidator = z.object({
+  name: z.string(),
+  description: z.string(),
+  imageUrl: z.string(),
+  rubricType: z.enum(['time-only', 'time-value']),
+  dueDate: z.number().optional(),
+  mission: z.string().optional(),
+  usdPerBaseShare: z.number().optional(),
+  rubricDescription: z
+    .tuple([z.string(), z.string(), z.string(), z.string(), z.string()])
+    .optional(),
+  links: z.array(ProjectLinkValidator).optional(),
+  parentDAOInfo: z
+    .object({
+      name: z.string().optional(),
+      address: z.string().optional(),
+      description: z.string().optional(),
+      imageUrl: z.string().optional(),
+    })
+    .optional(),
+});
+export type ProjectMetadata = z.infer<typeof ProjectMetadataValidator>;
 
-export type UpdatePercs = EventData & {
-  type: 'updatePercs';
-  updatePercsFrom: [number, number, number, number, number];
-  updatePercsTo: [number, number, number, number, number];
-};
+const ClaimValidator = EventValidator.extend({
+  type: z.literal('claim'),
+  metadata: z.union([z.literal('Corrupt'), ClaimMetadataValidator]),
+  totalSecondsWorked: z.string(),
+  totalAmountClaimed: z.string(),
+  sessionsTime: z.array(z.string()),
+  sessionsValue: z.array(z.number()),
+});
+export type Claim = z.infer<typeof ClaimValidator>;
 
-export type UpdateTPS = EventData & {
-  type: 'updateTokenPerSecond';
-  updateTPSFrom: string;
-  updateTPSTo: string;
-};
+const SummonShamanValidator = EventValidator.extend({
+  type: z.literal('summon'),
+  projectMetadata: z.union([z.literal('Corrupt'), ProjectMetadataValidator]),
+});
+export type SummonShaman = z.infer<typeof SummonShamanValidator>;
 
-export type UpdateInterval = EventData & {
-  type: 'updateInterval';
-  updateIntervalFrom: string;
-  updateIntervalTo: string;
-};
+const UpdateLockValidator = EventValidator.extend({
+  type: z.literal('updateLock'),
+  isLocked: z.boolean(),
+});
+export type UpdateLock = z.infer<typeof UpdateLockValidator>;
 
-export type TimelineEvent =
-  | Claim
-  | SummonShaman
-  | UpdateLock
-  | Mutiny
-  | UpdatePercs
-  | UpdateTPS;
+const MutinyValidator = EventValidator.extend({
+  type: z.literal('mutiny'),
+  mutinyFrom: z.string(),
+  mutinyTo: z.string(),
+});
+export type Mutiny = z.infer<typeof MutinyValidator>;
 
-export type ProjectMetadata = {
-  name: string;
-  description: string;
-  imageUrl: string;
-  mission?: string;
-  usdPerBaseShare?: number;
-  rubricDescription?: [string, string, string, string, string];
-  links?: ProjectLink[];
-  dueDate?: number;
-  parentDAOInfo?: {
-    name?: string;
-    address?: string;
-    description?: string;
-    imageUrl?: string;
-  };
-};
+const UpdatePercsValidator = EventValidator.extend({
+  type: z.literal('updatePercs'),
+  updatePercsFrom: z.tuple([
+    z.number(),
+    z.number(),
+    z.number(),
+    z.number(),
+    z.number(),
+  ]),
+  updatePercsTo: z.tuple([
+    z.number(),
+    z.number(),
+    z.number(),
+    z.number(),
+    z.number(),
+  ]),
+});
+export type UpdatePercs = z.infer<typeof UpdatePercsValidator>;
+
+const UpdateTPSValidator = EventValidator.extend({
+  type: z.literal('updateTokenPerSecond'),
+  updateTPSFrom: z.string(),
+  updateTPSTo: z.string(),
+});
+export type UpdateTPS = z.infer<typeof UpdateTPSValidator>;
+
+const UpdateIntervalValidator = EventValidator.extend({
+  type: z.literal('updateInterval'),
+  updateIntervalFrom: z.string(),
+  updateIntervalTo: z.string(),
+});
+export type UpdateInterval = z.infer<typeof UpdateIntervalValidator>;
+
+const TimelineEventValidator = z.discriminatedUnion('type', [
+  ClaimValidator,
+  SummonShamanValidator,
+  UpdateLockValidator,
+  MutinyValidator,
+  UpdatePercsValidator,
+  UpdateTPSValidator,
+  UpdateIntervalValidator,
+]);
+export type TimelineEvent = z.infer<typeof TimelineEventValidator>;
